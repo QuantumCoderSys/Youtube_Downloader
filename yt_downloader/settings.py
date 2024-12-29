0fbc3e1c755ac80ba07758cf3505dd08e4dc5c4d
Sarthak Rawool <sarthakrawool09@gmail.com>
2024-12-29 19:51:07 +0530

    Minor fix

--- yt_downloader/settings.py
+++ yt_downloader/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-%g7^36f#tkskck+ui&0q1)m8do*7hk%z%om$1^6_lih8xc7dp^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.64']
 
 
 # Application definition
